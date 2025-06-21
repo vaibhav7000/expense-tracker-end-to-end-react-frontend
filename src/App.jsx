b16b2fe14b64f15911839e7268b0bb024e89e9f4
vaibhav chawla <vaibhav.chawla@roundglass.com>
2025-06-21 17:37:28 +0530

    react-routing-undersanding

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Routes, Route, Link, NavLink } from "react-router";
 import HeadingMemo from "./Components/Heading";
 import TotalExpenseMemo from "./Components/TotalExpense";
 import ExpenseWrapperMemo from "./Components/ExpenseWrapper";
@@ -49,3 +50,16 @@ function App() {
 }
 
 export default App;
+
+
+// By default React is used to create dynamic single page applications SPA ( website that only contains 1 page ), React does not support multiple page application (MPA) but there is a package that can be used with React to create multi-page application called react-router
+
+// Navigation between different pages like done in normal HTML is not possible with React
+
+// => Therefore "react-router" is used to create websites with React that will contains multiple pages
+
+// => react-router will locallly have that page and we will only the fly generates the HTML content for that
+
+// Means react-router creates the local pages and each page has it different react that will handle that
+
+// Different SYNTAX to create Routes / url of the React-application
